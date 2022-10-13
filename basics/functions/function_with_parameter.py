95fe9e6be65f5588226151f4deee2c54d3eed6f5
Quin Mundell <6mundq94@solent.ac.uk>
2022-10-13 12:29:57 +0100

    Finished function with parameter task SOL

---
+++ basics/functions/function_with_parameter.py
@@ -0,0 +1,12 @@
+def escape_by(plan):
+    if plan == "jumping over":
+        print("We cannot escape that way! The boulder is too big!")
+    elif plan == "running around":
+        print("We cannot escape that way! The boulder is moving too fast!")
+    elif plan == "going deeper":
+        print("That might just work! Let's go deeper!")
+
+
+escape_by("jumping over")
+escape_by("running around")
+escape_by("going deeper")
