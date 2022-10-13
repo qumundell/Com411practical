7e521e902a0e09dde577ffa9f01ffb38265a857d
Quin Mundell <6mundq94@solent.ac.uk>
2022-10-13 12:24:57 +0100

    Finished function_with_nesting task SOL

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,9 @@
+def identify():
+    u_input = input("What lies before us?\n")
+    if u_input == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine")
+
+
+identify()
