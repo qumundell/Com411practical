f475eebf9142a26dab215684eee0ed3c33114392
Quin Mundell <6mundq94@solent.ac.uk>
2022-10-27 12:02:05 +0100

    Finished simple list task

--- data/data_files_demo/data_files.py
+++ data/data_files_demo/data_files.py
@@ -5,15 +5,15 @@ import json
 #     for line in file:
 #         print(line, end=" ")
 
-# with open("demo.csv") as file:
-#     csv_reader = csv.reader(file)
-#     heading = next(csv_reader)
-#     print(heading)
-#     for row in csv_reader:
-#         print(row)
-#         print(f"The leader of {row[1]} is {row[2]}")
+with open("demo.csv") as file:
+    csv_reader = csv.reader(file)
+    heading = next(csv_reader)
+    print(heading)
+    for row in csv_reader:
+        print(row)
+        print(f"The leader of {row[1]} is {row[2]}")
 
-with open("demo.json") as file:
-    data = json.load(file)
-    for item in data:
-        print(f"The leader of {item['name']} is {item['leader']}, {item['code']}")
+# with open("demo.json") as file:
+#     data = json.load(file)
+#     for item in data:
+#         print(f"The leader of {item['name']} is {item['leader']}, {item['code']}")
---
+++ data/lists/simple_list.py
@@ -0,0 +1,10 @@
+def directions():
+    l_directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return l_directions
+
+
+def run():
+    print(directions())
+
+
+run()
